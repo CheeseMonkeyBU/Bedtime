fd60fed1f4dc66eee8b13c2eb208c8913ee1534f
i7621977 <i7621977@PC42672>
2018-11-08 17:06:48 +0000

    Camera work and player spawning

---
+++ Bedtime/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_playerGameObject;
+    List<GameObject> m_spawnPoints;
+
+    [SerializeField]
+    public int playerCount = 2;
+
+    void Start()
+    {
+        m_spawnPoints = new List<GameObject>();
+        // look through all objects and find any that are spawn points and add them to the list of spawn points
+        m_spawnPoints.AddRange(GameObject.FindGameObjectsWithTag("SpawnPoint"));
+
+        if (m_spawnPoints.Count <= 0)
+        {
+            Debug.LogError("No player spawn points found");
+        }
+
+        for(int i = 0; i < playerCount; ++i)
+        {
+            spawnPlayer();
+        }
+    }
+
+    GameObject spawnPlayer()
+    {
+        int spawnPointIndex = Random.Range(0, m_spawnPoints.Count - 1);
+
+        GameObject player = Instantiate(m_playerGameObject);
+
+        player.transform.position = m_spawnPoints[spawnPointIndex].transform.position;
+
+        return player;
+    }
+}
