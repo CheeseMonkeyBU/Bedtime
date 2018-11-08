15a8702e26f923d25dac0fc09a130a1fdc78d944
i7621977 <i7621977@PC42672>
2018-11-08 20:39:18 +0000

    Splitscreen Fixes

--- Bedtime/Assets/Scripts/Camera/ScreenController.cs
+++ Bedtime/Assets/Scripts/Camera/ScreenController.cs
@@ -9,13 +9,18 @@ public class ScreenController : MonoBehaviour
     [SerializeField]
     GameObject m_cameraPrefab;
 
-    //[SerializeField]
+    [SerializeField]
     List<GameObject> m_cameras;
 
     [SerializeField]
     Canvas m_canvas;
 
     float m_border = 0.005f;
+
+    bool m_isSizeTransitioning = false;
+    float m_transitionSpeed = 5.0f;
+
+    float m_viewportX;
 
 
     // Use this for initialization
@@ -34,13 +39,33 @@ public class ScreenController : MonoBehaviour
         }
         if (Input.GetKeyDown("2"))
         {
-            removeViewport(0);
-        }
-        if (Input.GetKeyDown("3"))
-        {
             m_cameras[0].GetComponent<CameraController>().setOrthoSize(30, 1.0f);
         }
+        if (Input.GetKeyDown("4"))
+        {
+            removeViewport(m_cameras[Random.Range(0, m_cameras.Count - 1)]);
+        }
+        if (Input.GetKeyDown("5"))
+        {
+            removeViewport(m_cameras[0]);
+        }
     }
+
+    public void setViewports()
+    {
+        StopCoroutine("addLerpViewportReset");
+        StopCoroutine("removeLerpViewportReset");
+        float viewportWidth = (1.0f - m_border) / (m_cameras.Count);
+        float viewportX = 0.0f;
+
+        viewportX = 0.0f;
+        for (int i = 0; i < m_cameras.Count; ++i)
+        {
+            m_cameras[i].GetComponent<Camera>().rect = new Rect(viewportX, 0, viewportWidth, 1);
+            viewportX = viewportX + viewportWidth + m_border;
+        }
+    }
+
 
     public Camera addViewport()
     {
@@ -49,45 +74,86 @@ public class ScreenController : MonoBehaviour
         int newestCamera = m_cameras.Count - 1;
         m_cameras[newestCamera].name = "Player_" + newestCamera + "_Camera";
 
-
-        resetViewportLayout();
+        StartCoroutine("addLerpViewportReset");
 
         Debug.Log("Number of Cameras: " + m_cameras.Count);
         return m_cameras[newestCamera].GetComponent<Camera>();
     }
 
-    public void removeViewport(int _index)
+    public void removeViewport(GameObject _camera)
     {
-        m_cameras.RemoveAt(_index);
-        resetViewportLayout();
+        if(m_cameras.Count > 1)
+        {
+            Destroy(_camera);
+            m_cameras.Remove(_camera);
+            StopCoroutine("removeLerpViewportReset");
+            StartCoroutine("removeLerpViewportReset", _camera);
+        }
     }
 
-    void resetViewportLayout()
+    IEnumerator addLerpViewportReset()
     {
+        m_isSizeTransitioning = true;
+
+        float elapsedTime = 0.0f;
+        float startViewportWidth = (1.0f - m_border) / (m_cameras.Count - 1);
+        float endViewportWidth = (1.0f - m_border) / (m_cameras.Count);
         float viewportX = 0.0f;
 
-        float border = m_border;
-        if(m_cameras.Count <= 1)
+        while (elapsedTime < m_transitionSpeed)
         {
-            border = 0.0f;
-        }
-        float viewportWidth = (1.0f - border) / (m_cameras.Count);
+            viewportX = 0.0f;
+            for (int i = 0; i < m_cameras.Count; ++i)
+            {
+                float lerpedWidth = Mathf.SmoothStep(startViewportWidth, endViewportWidth, (elapsedTime / m_transitionSpeed));
 
-        // loop through all cameras to space them all out now there is a new one
-        for (int i = 0; i < m_cameras.Count; ++i)
-        {
-            // grab the camera component
-            Camera currentCamera = m_cameras[i].GetComponent<Camera>();
-            // set the viewport position
-            currentCamera.rect = new Rect(viewportX, 0, viewportWidth, 1);
+                m_cameras[i].GetComponent<Camera>().rect = new Rect(viewportX, 0, lerpedWidth, 1);
+                viewportX = viewportX + lerpedWidth + m_border;
+                elapsedTime += Time.deltaTime;
+            }
 
-            
-
-
-            // offset the viewport X for the next camera
-            viewportX = viewportX + viewportWidth + border;
+            yield return new WaitForEndOfFrame();
         }
 
-        GL.Clear(true, true, new Color(0, 0, 0, 1), 1.0f);
+        m_isSizeTransitioning = false;
+    }
+
+    // I dont know how this works but it does
+    IEnumerator removeLerpViewportReset()
+    {
+        m_isSizeTransitioning = true;
+
+        float elapsedTime = 0.0f;
+        float startViewportWidth = (1.0f - m_border) / (m_cameras.Count + 1);
+        float endViewportWidth = (1.0f - m_border) / (m_cameras.Count);
+
+        float endViewportX = 0.0f;
+
+        List<float> cameraStartViewportX = new List<float>();
+        for(int i = 0; i < m_cameras.Count; ++i)
+        {
+            cameraStartViewportX.Add(m_cameras[i].GetComponent<Camera>().rect.x);
+            Debug.Log(cameraStartViewportX[i]);
+        }
+
+        while (elapsedTime < m_transitionSpeed)
+        {
+            endViewportX = 0.0f;
+
+            // for each camera
+            for (int i = 0; i < m_cameras.Count; ++i)
+            {
+                float lerpedWidth = Mathf.SmoothStep(startViewportWidth, endViewportWidth, (elapsedTime / m_transitionSpeed));
+                float lerpedViewportX = Mathf.SmoothStep(cameraStartViewportX[i], endViewportX, (elapsedTime / m_transitionSpeed));
+
+                m_cameras[i].GetComponent<Camera>().rect = new Rect(lerpedViewportX, 0, lerpedWidth, 1);
+                endViewportX = endViewportX + endViewportWidth + m_border;
+                elapsedTime += Time.deltaTime;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        m_isSizeTransitioning = false;
     }
 }
